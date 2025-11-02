b1bec85b0bda3297ae8d214181587deb52f55cc9
brendanobilo <brendanxure@outlook.com>
2025-11-02 18:57:11 -0500

    update

--- Controllers/ExpensesController.cs
+++ Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceApp.Data;
+using FinanceApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceApp.Controllers
@@ -18,6 +19,17 @@ namespace FinanceApp.Controllers
         }
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+
+            }
             return View();
         }
     }
