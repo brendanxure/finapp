415ebf4047ac203a38aa64a46bed469f1f25a18c
brendanobilo <brendanxure@outlook.com>
2025-11-15 13:56:58 -0500

    update

---
+++ Data/Service/ExpensesService.cs
@@ -0,0 +1,17 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Service
+{
+    public class ExpensesService : IExpensesService
+    {
+        public Task Add(Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Expense>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
