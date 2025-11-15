d264b5ba5b07f59973c91ea05560ddf778fc6025
brendanobilo <brendanxure@outlook.com>
2025-11-15 14:01:01 -0500

    update

--- Data/Service/ExpensesService.cs
+++ Data/Service/ExpensesService.cs
@@ -4,6 +4,11 @@ namespace FinanceApp.Data.Service
 {
     public class ExpensesService : IExpensesService
     {
+        private readonly FinanceAppContext _context;
+        public ExpensesService(FinanceAppContext context)
+        {
+            _context = context;
+        }
         public Task Add(Expense expense)
         {
             throw new NotImplementedException();
